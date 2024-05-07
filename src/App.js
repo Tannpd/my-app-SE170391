0c7429037b0a42fc71bd2bc6ce76f1c8cc325947
Admin <you@example.com>
2024-05-07 19:44:45 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>FullName: Phan Đức Tấn</p>
+        <p>Class: SE17D08</p>
+        <p>School: FPT UNIVERSITY</p>
+        <p>Description: I am 20 years old. My major is SE. I live in Da Nang city.</p>
       </header>
     </div>
   );
